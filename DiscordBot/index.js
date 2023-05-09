d01acb3814f922326f5dd88657aba7b3ab01e540
ChilimanTube <chilimantube@gmail.com>
2023-05-09 16:58:06 +0200

    slash commands yes

--- DiscordBot/index.js
+++ DiscordBot/index.js
@@ -4,6 +4,7 @@ const { Client, GatewayIntentBits, Partials, Events, SlashCommandBuilder, Collec
 const {token} = require('./config.json');
 const { REST } = require('@discordjs/rest');
 const { helpEmbed } = require('./Commands/general/help.js');
+const createRoom = require('./commands/lfg/create-room');
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds,
@@ -40,35 +41,35 @@ new SlashCommandBuilder()
     .setName('help')
     .setDescription('Shows a list of commands');
 
-const lfg = new SlashCommandBuilder()
-    .setName('create-room')
-    .setDescription('Creates a LFG room')
+// const lfg = new SlashCommandBuilder()
+//     .setName('create-room')
+//     .setDescription('Creates a LFG room')
     
-    .addStringOption(option =>
-         option.setName('name')
-         .setDescription('The name of the room')
-         .setRequired(true))
+//     .addStringOption(option =>
+//          option.setName('name')
+//          .setDescription('The name of the room')
+//          .setRequired(true))
     
-    .addNumberOption(option2 =>
-        option2.setName('max-players')
-        .setDescription('The maximum number of players in the voice channel')
-        .setRequired(false))
+//     .addNumberOption(option2 =>
+//         option2.setName('max-players')
+//         .setDescription('The maximum number of players in the voice channel')
+//         .setRequired(false))
 
-    .addStringOption(option3 =>
-        option3.setName('game')
-        .setDescription('The game you are playing')
-        .setRequired(true))
+//     .addStringOption(option3 =>
+//         option3.setName('game')
+//         .setDescription('The game you are playing')
+//         .setRequired(true))
     
-    .addNumberOption(option4 =>
-        option4.setName('number-of-players')
-        .setDescription('The number of players you are looking for')
-        .setRequired(true));
+//     .addNumberOption(option4 =>
+//         option4.setName('number-of-players')
+//         .setDescription('The number of players you are looking for')
+//         .setRequired(true));
 
 
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    const activityName = ["/help"];
+    const activityName = ["DO NOT USE, BOT IS IN DEVELOPMENT"];
     client.user.setPresence({ activities: [{ name: activityName[Math.floor(Math.random() * activityName.length)] }], status: 'online' , type: "WATCHING"});
 
     client.application.commands.create({
@@ -79,6 +80,42 @@ client.on('ready', () => {
     client.application.commands.create({
         name:'help',
         description: 'User Guide',
+    });
+
+    client.application.commands.create({
+        name:'get-id',
+        description: 'Retrieves YouTube Channel ID',
+    });
+
+    client.application.commands.create({
+        name:'create-room',
+        description: 'Creates a LFG room',
+        options: [
+            {
+                name: 'name',
+                description: 'The name of the room',
+                type: 'STRING',
+                required: true,
+            },
+            {
+                name: 'max-players',
+                description: 'The maximum number of players in the voice channel',
+                type: 'NUMBER',
+                required: false,
+            },
+            {
+                name: 'game',
+                description: 'The game you are playing',
+                type: 'STRING',
+                required: true,
+            },
+            {
+                name: 'number-of-players',
+                description: 'The number of players you are looking for',
+                type: 'NUMBER',
+                required: true,
+            },
+        ],
     });
 });
 
@@ -94,6 +131,19 @@ client.on(Events.InteractionCreate, async interaction => {
         await interaction.reply({ embeds: [helpEmbed] }); 
     }
 
+    if (interaction.commandName === 'get-id') {
+        await interaction.reply({ content: 'ID Retrieved.' });
+    }
+
+    if (interaction.commandName === 'create-room') {
+        const { name, maxPlayers, game, numberOfPlayers } = interaction.options;
+        createRoom(interaction, name, maxPlayers, game, numberOfPlayers);
+        console.log(name, maxPlayers, game, numberOfPlayers);
+        console.log('Room Created');
+        await interaction.reply({
+          content: `Room Created: ${name} ${maxPlayers} ${game} ${numberOfPlayers}`,
+        });
+      }
     console.log(interaction);
 });
 
