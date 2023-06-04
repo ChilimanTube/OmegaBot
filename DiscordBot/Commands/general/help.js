8248d3182c0b9bd6f64a08c82f5cf67bbfe79b2d
ChilimanTube <chilimantube@gmail.com>
2023-06-04 20:42:08 +0200

    Embed message fix

--- DiscordBot/Commands/general/help.js
+++ DiscordBot/Commands/general/help.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 const author = {
     name: 'Chili',
@@ -12,7 +13,7 @@ const helpEmbed = new Discord.EmbedBuilder()
     .setTitle('OmegaBot')
     //.setURL('https://kousek-stranky.cwute.systems')
     .setDescription('Utility bot for The Blindcraft Discord server')
-    .setThumbnail('https://cdn.discordapp.com/attachments/1047406730331553842/1047410032154779698/38F7A04B-50DC-48CB-8386-60B497E1298A.jpg')
+    .setThumbnail('https://cdn.discordapp.com/attachments/982585395839660042/1114932860902719509/SmallStar.png')
     .addFields(
         { name: 'Help Guide', value: '## Interactions \n## Commands with ! prefix'},
         { name: '\u200B', value: '\u200B' , inline: true},
@@ -22,4 +23,6 @@ const helpEmbed = new Discord.EmbedBuilder()
     .setTimestamp()
     .setFooter({text: 'OmegaBot by Chili'});
 
-module.exports = helpEmbed;
+    module.exports = {
+        helpEmbed,
+    };
